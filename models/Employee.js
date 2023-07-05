abaa44d467a639c68cd6dd9944d7e8721480e294
BrunoCvlh <brunocarv4519@gmail.com>
2023-07-04 23:39:13 -0300

    new commit

--- models/Employee.js
+++ models/Employee.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const employeeSchema = new Schema({
-  name: {
-    type: String
-  },
-  email: {
-    type: String
-  }
+  name: {type: String},
+  email: {type: String}
 }, {timestamps: true})
 
 const Employee = mongoose.model('Employee', employeeSchema)
--- routes/employee.js
+++ routes/employee.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
-
+const EmployeeModel = require('../models/Employee')
 const EmployeeController = require('../controllers/EmployeeController')
+
 
 
 router.get('/', EmployeeController.index)
 router.post('/show', EmployeeController.show)
 router.post('/store', EmployeeController.store)
+router.get('/store', (req, res) => {
+    res.render('index.ejs')
+})
 router.post('/update', EmployeeController.update)
 router.post('/delete', EmployeeController.destroy)
 
